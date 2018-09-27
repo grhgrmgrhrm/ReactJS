6c14a6d795ee1d295901213bffa515d24bdf39e4
grhgrmgrhrm <grhgrmgrhrm@gmail.com>
2018-09-27 15:10:57 +0300

    webpack.config

---
+++ hw3/webpack.config.js
@@ -0,0 +1,18 @@
+//webpack 4
+const path = require('path');
+
+module.exports = {
+    entry: {main: './src/App.js'},
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader" }
+        ]
+    }
+};
